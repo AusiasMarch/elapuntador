3b030a3e95ac76addf138901cbd9efb788ef9789
Ausias <ausias@dribia.com>
2019-10-29 15:56:00 +0100

    Test.

--- install.sh
+++ install.sh
@@ -1,22 +1,33 @@
-sudo apt update
-sudo apt upgrade
+set -a
+source backend.env
+source env-postgres.env
+set +a
+
+
+sudo dpkg-reconfigure locales
+sudo echo "LC_ALL=en_US.UTF-8" | sudo tee -a /etc/environment
+sudo echo "LANG=en_US.UTF-8 UTF-8" | sudo tee -a /etc/environment
+
 
 sudo apt install -y python3
 sudo apt install -y python3-pip
 
 
 sudo apt install -y nginx
+sudo cp nginx_mi_apio.conf /etc/nginx/sites-available
+
+# Hardcoded ports and names
+sudo ln -s /etc/nginx/sites-available/nginx_mi_apio.conf /etc/nginx/sites-enabled/nginx_mi_apio
 
 # Get ssl certificates https://certbot.eff.org/lets-encrypt/pip-nginx
-sudo apt-get install certbot
+sudo apt-get install -y certbot
 sudo certbot certonly --authenticator standalone -d $SERVER_NAME --pre-hook "service nginx stop" --post-hook "service nginx start"
 echo '0 0 1 * * sudo certbot renew --pre-hook "service nginx stop" --post-hook "service nginx start"' | sudo tee -a /etc/crontab > /dev/null
 
 
-
-sudo apt install postgresql-$POSTGRES_VERSION libpq-dev postgresql-client postgresql-client-common
-sudo echo "host all all 192.168.1.0/24 trust" >> /etc/postgresql/$POSTGRES_VERSION/main/pg_hba.conf
-sudo sed "/^#listen_addresses*/listen_addresses = '127.0.0.1,192.168.1.26'" /etc/postgresql/$POSTGRES_VERSION/main/postgresql.conf > /etc/postgresql/$POSTGRES_VERSION/main/postgresql.conf
+sudo apt install -y postgresql-$POSTGRES_VERSION libpq-dev postgresql-client postgresql-client-common
+echo "host all all 192.168.1.0/24 trust" | sudo tee -a /etc/postgresql/$POSTGRES_VERSION/main/pg_hba.conf
+sudo sed -i "/^#listen_addresses.*/a listen_addresses\ =\ '127.0.0.1,192.168.1.26'" /etc/postgresql/$POSTGRES_VERSION/main/postgresql.conf
 sudo systemctl restart postgresql
 sudo su postgres -c "psql  -c \"CREATE USER $POSTGRES_USER WITH PASSWORD '$POSTGRES_PASSWORD';\""
 sudo su postgres -c "psql  -c \"CREATE DATABASE $POSTGRES_USER OWNER $POSTGRES_USER;\""
