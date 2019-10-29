390c26e71351ab062c5be0b6fb84ca6716814201
Ausias <ausias@dribia.com>
2019-10-29 06:40:10 +0100

    Test.

--- app/db_models/reporter.py
+++ app/db_models/reporter.py
@@ -7,4 +7,7 @@ from db.base_class import Base
 class Reporter(Base):
     id = Column(Integer, primary_key=True, index=True)
     full_name = Column(String, index=True)
-    relation = relationship("relation", back_populates="reporter")
+    relation_id = Column(int, ForeignKey="relation.id")
+    
+    pesos = relationship("Peso", back_populates="reporter")
+    relation = relationship("Relation", back_populates="reporters")
