3562d3bbd4dd8b0ff4619d49365ebc4bf0f0acff
Ausias <ausias@dribia.com>
2019-10-29 16:58:37 +0100

    Test.

--- execute.sh
+++ execute.sh
@@ -1,3 +1,4 @@
 source <(grep -v '^#' env-postgres.env | sed -E 's|^(.+)=(.*)$|: ${\1=\2}; export \1|g')
 source <(grep -v '^#' backend.env | sed -E 's|^(.+)=(.*)$|: ${\1=\2}; export \1|g')
-uvicorn app/main:app --host 127.0.0.1 --port $INTERNAL_PORT
+cd app
+uvicorn main:app --host 127.0.0.1 --port $INTERNAL_PORT
