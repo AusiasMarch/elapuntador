eb87fe369c89c88c9d8a73b0cde2b820266d2ec9
Ausias <ausias@dribia.com>
2019-10-30 17:29:37 +0100

    Test.

--- app/api/api_v1/endpoints/utils.py
+++ app/api/api_v1/endpoints/utils.py
@@ -3,23 +3,11 @@ from pydantic.types import EmailStr
 
 
 from api.utils.security import get_current_active_superuser
-from core.celery_app import celery_app
 from models.msg import Msg
 from models.user import UserInDB
 from myemails import send_test_email
 
 router = APIRouter()
-
-
-@router.post("/test-celery/", response_model=Msg, status_code=201)
-def test_celery(
-    msg: Msg, current_user: UserInDB = Depends(get_current_active_superuser)
-):
-    """
-    Test Celery worker.
-    """
-    celery_app.send_task("app.tasks.test.test_celery", kwargs={"word": msg.msg})
-    return {"msg": "Word received"}
 
 
 @router.post("/test-email/", response_model=Msg, status_code=201)
