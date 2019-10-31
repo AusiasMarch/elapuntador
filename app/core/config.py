825dc7c809ca29070903b4129761f5c8af55049c
Ausias <ausias@dribia.com>
2019-10-31 15:53:02 +0100

    Test.

--- app/core/config.py
+++ app/core/config.py
@@ -9,7 +9,7 @@ for env_file in env_files:
 
 
 def get_env_variable(var_name):
-    variable = os.getenv(var_name)
+    variable = get_env_variable(var_name)
     if variable is not None:
         return variable
     else:
@@ -36,7 +36,7 @@ ACCESS_TOKEN_EXPIRE_MINUTES = 60 * 24 * 8  # 60 minutes * 24 hours * 8 days = 8 
 
 SERVER_NAME = get_env_variable("SERVER_NAME")
 SERVER_HOST = get_env_variable("SERVER_HOST")
-BACKEND_CORS_ORIGINS = os.getenv(
+BACKEND_CORS_ORIGINS = get_env_variable(
     "BACKEND_CORS_ORIGINS"
 )  # a string of origins separated by commas, e.g: "http://localhost, http://localhost:4200, http://localhost:3000, http://localhost:8080, http://local.dockertoolbox.tiangolo.com"
 PROJECT_NAME = get_env_variable("PROJECT_NAME")
@@ -52,13 +52,13 @@ SQLALCHEMY_DATABASE_URI = (
 
 SMTP_TLS = getenv_boolean("SMTP_TLS", True)
 SMTP_PORT = None
-_SMTP_PORT = os.getenv("SMTP_PORT")
+_SMTP_PORT = get_env_variable("SMTP_PORT")
 if _SMTP_PORT is not None:
     SMTP_PORT = int(_SMTP_PORT)
-SMTP_HOST = os.getenv("SMTP_HOST")
-SMTP_USER = os.getenv("SMTP_USER")
-SMTP_PASSWORD = os.getenv("SMTP_PASSWORD")
-EMAILS_FROM_EMAIL = os.getenv("EMAILS_FROM_EMAIL")
+SMTP_HOST = get_env_variable("SMTP_HOST")
+SMTP_USER = get_env_variable("SMTP_USER")
+SMTP_PASSWORD = get_env_variable("SMTP_PASSWORD")
+EMAILS_FROM_EMAIL = get_env_variable("EMAILS_FROM_EMAIL")
 EMAILS_FROM_NAME = PROJECT_NAME
 EMAIL_RESET_TOKEN_EXPIRE_HOURS = 48
 EMAIL_TEMPLATES_DIR = "/app/app/email-templates/build"
