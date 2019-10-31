a65bcb29a78d5a7055fe2a71b3ef03ef7a92d703
Ausias <ausias@dribia.com>
2019-10-31 22:17:03 +0100

    Test.

--- app/main.py
+++ app/main.py
@@ -28,21 +28,50 @@ if config.BACKEND_CORS_ORIGINS:
 app.include_router(api_router, prefix=config.API_V1_STR)
 
 
+from starlette.authentication import (
+    AuthenticationBackend, AuthenticationError, SimpleUser, UnauthenticatedUser,
+    AuthCredentials
+)
+from starlette.middleware.authentication import AuthenticationMiddleware
+from starlette.responses import PlainTextResponse
+import base64
+import binascii
+class BasicAuthBackend(AuthenticationBackend):
+    async def authenticate(self, request):
+        if "Authorization" not in request.headers:
+            return
+
+        auth = request.headers["Authorization"]
+        try:
+            scheme, credentials = auth.split()
+            if scheme.lower() != 'basic':
+                return
+            decoded = base64.b64decode(credentials).decode("ascii")
+        except (ValueError, UnicodeDecodeError, binascii.Error) as exc:
+            raise AuthenticationError('Invalid basic auth credentials')
+
+        username, _, password = decoded.partition(":")
+        # TODO: You'd want to verify the username and password here,
+        #       possibly by installing `DatabaseMiddleware`
+        #       and retrieving user information from `request.database`.
+        return AuthCredentials(["authenticated"]), SimpleUser(username)
+from starlette.middleware.authentication import AuthenticationMiddleware
+app.add_middleware(AuthenticationMiddleware, backend=BasicAuthBackend())
+
 @app.middleware("http")
 async def db_session_middleware(request: Request, call_next):
     print(dir(request))
-    # print('request.auth')
-    # print(request.auth)
+    print('request.auth')
+    print(request.auth)
     print('request.body')
-    print(request.body)
+    print(request.body())
     print('request.form')
-    print(request.form)
+    print(request.form())
     print('request.headers')
     print(request.headers)
     print('request.json')
-    print(request.json)
-    print('request.session')
-    print(request.session)
+    print(request.json())
+    # print('request.session')
     print('request.user')
     print(request.user)
     
