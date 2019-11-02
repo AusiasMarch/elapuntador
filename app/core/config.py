a3442c10ebe26d079c370a37a2b455dd4d215fb8
Ausias <ausias@dribia.com>
2019-11-02 18:34:48 +0100

    Test.

--- app/core/config.py
+++ app/core/config.py
@@ -2,8 +2,9 @@ import os
 from environs import Env
 
 env = Env()
-print(os.listdir('.'))
+
 env_files = [x for x in os.listdir('.') if x.endswith('.env')]
+print(env_files)
 env_files = [x for x in env_files if '_{}'.format(x) not in env_files]
 for env_file in env_files:
     print("Loading {} file.".format(env_file))
