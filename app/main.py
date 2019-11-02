9bd437ef17ba4a537b9cadcb2bd577ff1d448b43
Ausias <ausias@dribia.com>
2019-11-02 18:14:19 +0100

    Test.

--- app/main.py
+++ app/main.py
@@ -47,7 +47,7 @@ async def is_google_middleware(request: Request, call_next):
         decoded_token = jwt.decode_google_token(id_token)
         print(decoded_token['aud'])
         print(config.GOOGLE_CLIENTID)
-        print(config.SECRET_KEY)
+        print(config.GOOGLE_ISS)
         if decoded_token['aud'] != config.GOOGLE_CLIENTID:
             raise AuthenticationError('Invalid Google Client ID.')
         if decoded_token['iss'] != config.GOOGLE_ISS:
