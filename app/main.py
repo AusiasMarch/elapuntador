7babe7f24e3ec1d2dd962183e60e26f6fa51c767
Ausias <ausias@dribia.com>
2019-11-02 20:12:31 +0100

    Test

--- app/main.py
+++ app/main.py
@@ -40,24 +40,24 @@ async def db_session_middleware(request: Request, call_next):
     return response
 
 
-@app.middleware("http")
-async def is_google_middleware(request: Request, call_next):
-    user_agent = request.headers['user-agent']
-    if user_agent == 'Google-Dialogflow':
-        body = await request.json()
-        body2 = await request.body()
-        print(type(body))
-        print(body)
-        print(body2)
-        print(type(body))
-        id_token = body['originalDetectIntentRequest']['payload']['user']['idToken']
-        decoded_token = jwt.decode_google_token(id_token)
-        if decoded_token['aud'] != config.GOOGLE_CLIENTID:
-            raise AuthenticationError('Invalid Google Client ID.')
-        if decoded_token['iss'] != config.GOOGLE_ISS:
-            raise AuthenticationError('Invalid Google Token ID iss.')
-        request.state.email = decoded_token['email']
-        
-    response = await call_next(request)
-    print('b')
-    return response
+# @app.middleware("http")
+# async def is_google_middleware(request: Request, call_next):
+#     user_agent = request.headers['user-agent']
+#     if user_agent == 'Google-Dialogflow':
+#         body = await request.json()
+#         body2 = await request.body()
+#         print(type(body))
+#         print(body)
+#         print(body2)
+#         print(type(body))
+#         id_token = body['originalDetectIntentRequest']['payload']['user']['idToken']
+#         decoded_token = jwt.decode_google_token(id_token)
+#         if decoded_token['aud'] != config.GOOGLE_CLIENTID:
+#             raise AuthenticationError('Invalid Google Client ID.')
+#         if decoded_token['iss'] != config.GOOGLE_ISS:
+#             raise AuthenticationError('Invalid Google Token ID iss.')
+#         request.state.email = decoded_token['email']
+#
+#     response = await call_next(request)
+#     print('b')
+#     return response
