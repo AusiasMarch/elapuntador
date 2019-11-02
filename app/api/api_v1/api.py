0e994da8106e9effd527de3f52801694d2cdde68
Ausias <ausias@dribia.com>
2019-11-02 23:51:38 +0100

    Test

--- app/api/api_v1/api.py
+++ app/api/api_v1/api.py
@@ -1,7 +1,7 @@
 from fastapi import APIRouter
 
 from api.api_v1.endpoints import (
-    altura,
+    apuntes,
     login,
     users,
     utils,
@@ -13,5 +13,5 @@ api_router.include_router(login.router, tags=["login"])
 api_router.include_router(users.router, prefix="/users", tags=["users"])
 api_router.include_router(utils.router, prefix="/utils", tags=["utils"])
 # DRIBIA
-api_router.include_router(peso.router, prefix="/insert", tags=["peso"])
+api_router.include_router(apuntes.router, prefix="/apuntes", tags=["apuntes", "peso", "altura"])
 api_router.include_router(peso.router, prefix="/peso", tags=["peso"])
