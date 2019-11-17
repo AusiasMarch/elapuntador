d325f62e574585e7a22f3fcef8547c25945f4d20
Ausias <ausias@dribia.com>
2019-11-17 16:37:05 +0100

    Test.

--- app/api/api_v1/api.py
+++ app/api/api_v1/api.py
@@ -9,9 +9,12 @@ from api.api_v1.endpoints import (
 )
 
 api_router = APIRouter()
-api_router.include_router(login.router, tags=["login"])
-api_router.include_router(users.router, prefix="/users", tags=["users"])
-api_router.include_router(utils.router, prefix="/utils", tags=["utils"])
+
+
 # DRIBIA
 api_router.include_router(apuntar.router, prefix="/apuntar", tags=["apuntar"])
 api_router.include_router(dash.router, prefix="/dash", tags=["dash"])
+
+api_router.include_router(login.router, tags=["login"])
+api_router.include_router(users.router, prefix="/users", tags=["users"])
+api_router.include_router(utils.router, prefix="/utils", tags=["utils"])
